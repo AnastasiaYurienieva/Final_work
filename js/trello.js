4e203dbd9f8c9fe9d31e01a83c8f3f9c383aade6
AnastasiaYurienieva <persefonaweiss@gmail.com>
2024-10-24 19:47:54 +0300

    trello

--- js/trello.js
+++ js/trello.js
@@ -3,8 +3,26 @@ const wrapper = document.createElement('div');
 wrapper.classList.add('container');
 body.appendChild(wrapper);
 
+const wrapTrello = document.createElement('div');
+wrapTrello.classList.add('wrap-trello');
+wrapper.appendChild(wrapTrello);
 
+const text = document.createElement('p');
+text.innerText = "Trello";
+text.classList.add('text');
+wrapTrello.appendChild(text);
 
+const timer = document.createElement('time');
+timer.classList.add('timer');
+wrapTrello.appendChild(timer);
+
+function updateTime() {
+    const now = new Date();
+    timer.textContent = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+};
+
+let intervalId;
+intervalId = setInterval(updateTime, 1000);
 
 const wrapforCards = document.createElement('div');
 wrapforCards.classList.add('wrap-for-cards');
